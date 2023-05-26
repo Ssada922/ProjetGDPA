08ac312c3d6abe8d722f84088f64b8f2b9e16e7f
Ssada_922 <132320327+Ssada922@users.noreply.github.com>
2023-05-26 22:44:29 +0000

    Connexion.php

--- Connexion.php
+++ Connexion.php
@@ -1,7 +1,5 @@
-<?php  
-//Initialisation de la session
-session_start();
-?>
+<?php //Initialisation de la session
+       session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,45 +9,54 @@ session_start();
 </head>
 <body>
 <?php
-   //Connexion a la base de donnee
-   require('configuration.php'); 
-
+  //Connexion a la base de donnee
+   require('configuration.php');
+  
        if (isset($_POST['phone']) && !empty($_POST['phone'])){ 
            $phone = htmlspecialchars($_POST['phone']);
-           $phone = mysqli_real_escape_string($conn, $phone);
+           $phone = stripslashes($phone);
            $_SESSION['phone'] = $phone;
            $password = htmlspecialchars($_POST['password']);
-           $password = mysqli_real_escape_string($conn, $password);
+           $password = stripslashes($password);
+
          //Requete de selection  du numero de telephone et du mot de passe de l'utilisateur(Admin/Personnel)
-           $req = "SELECT * FROM personnels WHERE phone='$phone' and password='$password'";/*".hash('sha256', $password)."*/
-  
-                $req1 = mysqli_query($conn,$req) or die(mysql_error());
-  
-            if (mysqli_num_rows($req1) == 1) {
+           $req = "SELECT * FROM personnels WHERE phone=:phone AND password= :password";/*".hash('sha256', $password)."*/
+           $reqtemp = $pdo->prepare($req);
+           $reqtemp->bindParam(':phone', $phone);
+           $reqtemp->bindParam(':password', $password);
+           $reqtemp->execute();
 
-                    $personnel = mysqli_fetch_assoc($req1);
+         //Verification des resultats de le requetes
+           $rowCount = $reqtemp->rowCount();   
+                  if($rowCount>0){
+                          $personnel =  $reqtemp->fetch(PDO::FETCH_ASSOC);
 
-                // vérifier si l'utilisateur est un administrateur ou un utilisateur
-                    if ($personnel['statut'] == 'admin') {
-                       
-                        header('location: Acceuil_Administrateur.php');
-                       
-                       }else{
-                        
+               //Verification:si l'utilisateur est admin ou personnel
+                        if($personnel['statut'] == 'admin'){
+
+                          header('location: Acceuil_Administrateur.php');
+
+                      }else{
+
                         header('location: AccueilPersonnel.php'); 
-                       
-             }
-       }else{
-                 $msgerreur = "Le nom d'utilisateur ou le mot de passe est incorrect.";
-    }
- }
+
+                      }
+
+             }else{
+                 
+              $msgerreur = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+
+              }
+
+      }
+           
 ?>
                 <form class="container" action="" method="post" name="login">
                         <h1 class="form-titre">Connexion</h1>
                     <input type="int" class="form-input" name="phone" placeholder="Numero telephone">
                     <input type="password" class="form-input" name="password" placeholder="Mot de passe">
-                    <input type="submit" value="Se Conneter" name="submit" class="form-button">
-                    <p class="form-phrase">Vous êtes nouveau ici?<a class="con" href="Inscription.php">S'inscrire</a></p>
+                    <input type="submit" value="Se Connecter" name="submit" class="form-button">
+                    <p class="form-phrase">Vous êtes nouveau ici?<a href="Inscription.php">S'inscrire</a></p>
 
            <?php if (! empty($msgerreur)) { ?>
     <p class="msgErreur"><?php echo $msgerreur; ?></p>
