62c9b7ec1270f16c4a79c759b0ae46142ee54f8c
Ssada_922 <132320327+Ssada922@users.noreply.github.com>
2023-06-11 13:28:42 +0000

    ajoutCategories.php

--- ajoutCategories.php
+++ ajoutCategories.php
@@ -9,7 +9,7 @@
         <title>Formulaire d'ajout de categories avec Popup</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="StyleformPupup.css">
+        <link rel="stylesheet" href="css/StyleformPupup.css">
     </head>
     <body>
         <?php
