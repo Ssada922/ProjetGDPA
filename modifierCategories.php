23f5533f11aef06edfa82b1b2fba0f80fc5d868d
Ssada_922 <132320327+Ssada922@users.noreply.github.com>
2023-06-12 15:24:03 +0000

    modifierCategories.php

--- modifierCategories.php
+++ modifierCategories.php
@@ -25,8 +25,8 @@ $description ='';
                          $updateCat = "UPDATE categories SET libelle = ?, description = ? WHERE id_cat = ?";
                          $updateCat =  $pdo->prepare($updateCat);
                          $updateCat->execute(array($libelleSaisi, $descriptionSaisi, $recupId));
-
-                                       header('location:ajoutCategories.php');     
+                                         echo  " <p class='msgReussi'>La catégorie a été modifiée avec succès à la base de données.</p> ";
+                                         header("refresh:3;url=ajoutCategories.php");   
               }else{
                      echo"";
               }
@@ -44,18 +44,38 @@ $description ='';
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/StyleformPupup.css">
     </head>
+      
     <body>
-            <form class="formContainer" method="post" action="">
-                <h2 class="h1Form" >Ajout Catégories</h2>
-                <label class="formLab" for="lib">Libellé</label>
-                <input class="formInput" name="libelle" id="lib"  type="text"  placeholder="Votre libellé" value="<?=$libelle;?>" required>
-                <label class="formLab" for="desc">Description</label>
-                <textarea class="formInput" name="description" id="desc" cols="30" rows="10" ><?=$description;?></textarea>
-                <input class="butSubmit" type="submit" name="ajouter" value="Ajouter">
-                <input class="butSubmitFerm" type="submit" name="" value="Fermer">
-            </form>
-            <?php if(!empty($smsErr)){  ?>
-               <p class="msgErreur"><?php echo $smsErr; ?></p>
+               <script>
+                function ouverturePopup(){
+                    //Appliquons dans le document l'element qui a l'id(monPopup) la valeur block
+                    var afficher = document.getElementById("monPopup");
+                    afficher.style.display = "block";
+                }
+                    //Appliquons dans le document l'element qui a l'id(monPopup) la valeur none
+                function fermeturePopup(){
+                    var fermer = document.getElementById("monPopup");
+                    fermer.style.display = "none";
+                }
+               </script>
+            <div class="divButOuvrirPop">
+                <p>Cliquer sur le boutton "ouvrir le popup" pour modifier une Categorie.</p>
+                <button class="butOuvrirPop" onclick="ouverturePopup()"><strong>Ouvrir le popup</strong></button>
+            </div>
+
+            <div class="divForm" id="monPopup">
+               <form class="formContainer" method="post" action="">
+                   <h2 class="h1Form" >Ajout Catégories</h2>
+                   <label class="formLab" for="lib">Libellé</label>
+                   <input class="formInput" name="libelle" id="lib"  type="text"  placeholder="Votre libellé" value="<?=$libelle;?>" required>
+                   <label class="formLab" for="desc">Description</label>
+                   <textarea class="formInput" name="description" id="desc" cols="30" rows="10" ><?=$description;?></textarea>
+                   <input class="butSubmit" type="submit" name="ajouter" value="Ajouter">
+                   <input onclick="fermeturePopup()" class="butSubmitFerm" type="button" name="" value="Fermer">
+               </form>
+            </div>
+               <?php if(!empty($smsErr)){  ?>
+                 <p class="msgErreur"><?php echo $smsErr; ?></p>
                <?php } ?>
     </body>
 </html>
