892ac43c05e3fe081e70e38b2cd46711dd7e4610
Ssada_922 <132320327+Ssada922@users.noreply.github.com>
2023-07-04 05:45:16 +0000

    ajoutCategories.php

--- ajoutCategories.php
+++ ajoutCategories.php
@@ -1,6 +1,12 @@
 <?php
- session_start();
- ob_start();
+session_start();
+ob_start();
+ob_start();
+if(!isset($_SESSION['id_pers'])){
+    header("Location: Index.php");
+    exit(); 
+  }
+     $id_pers = $_SESSION['id_pers'];
  include_once('Configuration.php');
 ?>
 <!DOCTYPE html>
@@ -9,12 +15,12 @@
         <title>Formulaire d'ajout de categories avec Popup</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/StyleformPupup.css">
+        <link rel="stylesheet" href="StyleformPupup.css">
     </head>
     <body>
         <?php
         if(isset($_POST['ajouter'])){
-            if(isset($_POST['libelle']) && !empty($_POST['libelle']) && isset($_POST['description']) && $_POST['description']){
+            if(isset($_POST['libelle']) && !empty($_POST['libelle']) && isset($_POST['description']) && !empty($_POST['description'])){
                 $libelle = htmlspecialchars($_POST['libelle']);
                 $libelle = stripslashes($libelle);
                 $libelle = trim($libelle);
@@ -49,19 +55,19 @@
         
              <div class="divButOuvrirPop">
                 <p>Cliquer sur le boutton "ouvrir le popup" pour ajouter une Categorie.</p>
-                <button class="butOuvrirPop" onclick="ouvertureFormulaire()"><strong>Ouvrir le popup</strong></button>
+                <button class="butOuvrirPop" onclick="ouverturePopup()"><strong>Ouvrir le popup</strong></button>
              </div>
 
         <div class="divForm" id="monPopup">
-            <form class="formContainer" method="post" action="">
-                <h2 class="h1Form" >Ajout Catégories</h2>
-                <label class="formLab" for="lib">Libellé</label>
-                <input class="formInput" name="libelle" id="lib"  type="text"  placeholder="Votre libellé"  required>
-                <label class="formLab" for="desc">Description</label>
-                <textarea class="formInput" name="description" id="desc" cols="30" rows="10" ></textarea>
-                <input class="butSubmit" type="submit" name="ajouter" value="Ajouter">
-                <input class="butSubmitFerm" type="button" name="" value="Fermer" onclick="fermetureFormulaire()" >
-            </form>
+              <form class="formContainer" method="post" action="">
+               <h2 class="h1Form" >Ajout Catégories</h2>
+               <label class="formLab" for="lib">Libellé</label>
+               <input class="formInput" name="libelle" id="lib"  type="text"  placeholder="Votre libellé"  required>
+               <label class="formLab" for="desc">Description</label>
+               <textarea class="formInput" name="description" id="desc" cols="30" rows="10" ></textarea>
+               <input class="butSubmit" type="submit" name="ajouter" value="Ajouter">
+               <input onclick="fermeturePopup()" class="butSubmitFerm" type="button" name="" value="Fermer">
+              </form>
             <?php if(!empty($msgErr)){  ?>
                <p class="msgErreur"><?php echo $msgErr; ?></p>
                <?php } ?>
@@ -69,11 +75,11 @@
         <!--Script permettant l'ouverture et la fermeture du formulaire-->
         <script> 
           //Pour la fonction d'ouverture j'utilise diplay:block pour rendre visible en faite le formulaire
-            function ouvertureFormulaire() {
+            function ouverturePopup() {
               document.getElementById("monPopup").style.display = "block";
             }
         //Pour la fonction de fermeture j'utilise le le diplay:none pour masquer en faite le formulaire
-            function fermetureFormulaire() {
+            function fermeturePopup() {
               document.getElementById("monPopup").style.display = "none";
             }
           </script>
