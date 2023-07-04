84bd7549faa19a47aecddea6f87d59c63cf1bb12
Ssada_922 <132320327+Ssada922@users.noreply.github.com>
2023-07-04 05:54:37 +0000

    accueilPersonnel.php

--- accueilPersonnel.php
+++ accueilPersonnel.php
@@ -14,7 +14,7 @@ ob_start(); //Initialisation du tempon
         <meta charset="utf-8"  >  
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="Personnel.css">
+        <link rel="stylesheet" href="css/Personnel.css">
     </head>
       <body>
      
